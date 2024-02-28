bb296b0b51995b3fddd7c1222486ccbcb6235be4
madhuvr97 <73780001+madhuvr97@users.noreply.github.com>
2024-02-28 12:44:34 +0530

    Delete AddingNumbers_method1.py

--- AddingNumbers_method1.py
+++
@@ -1,8 +0,0 @@
-def add_numbers(*args):
-    result = sum(args)
-    return result
-
-numbers = input("enter the list of numbers you want to add seperated by a space: ")
-numbers_list = list(map(int, numbers.split()))
-add_2_numbers = add_numbers(*numbers_list)
-print(add_2_numbers)
