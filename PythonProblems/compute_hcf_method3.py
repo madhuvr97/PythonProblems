b07f343e5b894d621ddf7f6dc341ed591b90c729
madhuvr97 <vrmadhugowda96@gmail.com>
2024-03-04 22:31:03 +0530

    updated the code

--- PythonProblems/compute_hcf_method3.py
+++ PythonProblems/compute_hcf_method3.py
@@ -4,22 +4,22 @@ def compute_hcf(x,y):
     return x
 
 def get_positive_integer(prompt):
-    try:
         value = int(input(prompt))
-        if value < 0:
-            print("Please enter a positive integer.")
-        else:
-            return value
-    except ValueError:
-        print("Invalid input. Please enter an integer.")
+        return value
 
 try:
     x = get_positive_integer("Enter the first number: ")
+    if x < 0:
+        raise ValueError("Please enter a positive integer...")
     y = get_positive_integer("Enter the second number: ")
+    if y < 0:
+        raise ValueError("Please enter a positive integer...")
     if x == 0 and y == 0:
         print("HCF of 0 and 0 is undefined.")
     else:
         hcf = compute_hcf(x,y)
         print(f"The hcf of {x} and {y} is {hcf}")
-except Exception as exception:
-    print("An unknown error occurred:", str(exception))
+except ValueError as ve:
+    print("Invalid input: ", str(ve))
+except Exception as e:
+    print("An unknown error occurred: ", str(e))
