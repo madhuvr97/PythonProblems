86adab6887c9b40cb5294dff79e2449f6ec09222
madhuvr97 <vrmadhugowda96@gmail.com>
2024-03-11 20:14:41 +0530

    It's my 3rd portfolio project

---
+++ projects_low_complexity/merging_pdfs_method2.py
@@ -0,0 +1,42 @@
+import PyPDF2
+from PyPDF2 import PdfMerger
+import os
+
+def merge_pdfs(pdf_files, output_file):
+    merger = PdfMerger()
+
+    for pdf_file in pdf_files:
+        merger.append(pdf_file)
+
+    merger.write(output_file)
+    merger.close()
+
+def get_input_files():
+    pdf_files = []
+    while True:
+        file_path = input("Enter the file path of a PDF to merge (or type 'done' to finish): ")
+        if file_path == 'done'.lower():
+            break
+        elif not os.path.isfile(file_path):
+            print(f"Error: '{file_path}' is not a valid file path.")
+        else:
+            if file_path.lower().endswith('.pdf'):
+                pdf_files.append(file_path)
+            else:
+                print(f"Error: '{file_path}' is not a PDF file.")
+    return pdf_files
+
+def main():
+    input_files = get_input_files()
+    if input_files:
+        output_file = input("Enter the file path for the merged PDF file: ")
+        if os.path.isfile(output_file):
+            merge_pdfs(input_files, output_file)
+            print("The PDFs have been merged successfully.")
+        else:
+            print(f"Error: '{output_file}' path is not valid.")
+    else:
+        print("No PDFs to merge.")
+
+if __name__ == "__main__":
+    main()
