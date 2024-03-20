f9a7f5f5b16be55943882ca9e852b9a097a1f14d
madhuvr97 <vrmadhugowda96@gmail.com>
2024-03-20 16:37:36 +0530

    It's my 7th test case

---
+++ projects_low_complexity/selenium_projects/test_case_7(sorting_columns).py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+url = "https://rahulshettyacademy.com/seleniumPractise"
+
+# headless mode
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("headless")
+chrome_options.add_argument("--ignore-certificate-errors")
+
+# Initialize web driver
+driver = webdriver.Chrome(options= chrome_options)
+driver.maximize_window()
+
+try:
+    driver.get(url)
+    driver.find_element(By.LINK_TEXT, "Top Deals").click()
+
+    tabs_opened = driver.window_handles
+    driver.switch_to.window(tabs_opened[1])
+    driver.find_element(By.XPATH, "//th[@role='columnheader'][1]").click()
+    veg_fruits = driver.find_elements(By.XPATH, "//tbody/tr/td[1]")
+    sorted_veg_fruits = sorted(veg_fruits, key=lambda x: x.text)
+    assert veg_fruits == sorted_veg_fruits
+
+    '''
+    browser_sorted_list = []
+    for veg in veg_fruits:
+        browser_sorted_list.append(veg.text)
+    original_browser_sorted_list = browser_sorted_list.copy()
+    browser_sorted_list.sort()
+    assert browser_sorted_list == original_browser_sorted_list
+    '''
+
+except Exception as e:
+    print("An error occurred:", e)
+
+finally:
+    driver.quit()
+
